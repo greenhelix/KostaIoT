81974acc07119f8feaec53b579b55c09ca210287
global_ik <helloglobal@example.com>
2022-02-16 09:48:16 +0900

    프로젝트 명 변경 및 파일정리

--- HelloWorld/Step1HelloWorld/step01.cpp
+++
@@ -1,5 +0,0 @@
-#include <stdio.h>
-int main() {
-	printf("First\n");
-	return 0;
-}
--- HelloWorld/Step1HelloWorld/문자열.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-int main(void) {
-	/*문자열은 ""로 표시
-		문자는 ''로 표시*/
-	char str[6] = "korea";
-	print(str);
-}
