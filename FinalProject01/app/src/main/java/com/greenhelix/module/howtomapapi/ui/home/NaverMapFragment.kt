9b2226876c1ab0d882415b0225df75659bf6808c
Kevin <dlrghks4444@gmail.com>
2022-06-07 23:38:15 +0900

    InfoWindowAdapter inner class 로 어댑터 만들어줌 -> 데이터 연계 부분은 View model 로 분리 시작해야함 -> inner class 로 하면서 contain : ViewGroup 부분을 가져다 쓸 수 있었다. -> Destroy 부분에서 해당 inner class 를 따로 불러서 처리해주었다.
    onMapReady 부분
    -> naverMap apply 적용하여 코드량 축소
    -> infoWindow 변수는 따로 생성해서 관리해야 더 편한듯 하다.
    -> 좌표 마커 부분 중복 코드 줄일 생각해야함.

--- FinalProject01/app/src/main/java/com/greenhelix/module/howtomapapi/ui/home/NaverMapFragment.kt
+++ FinalProject01/app/src/main/java/com/greenhelix/module/howtomapapi/ui/home/NaverMapFragment.kt
@@ -1,23 +1,24 @@
 package com.greenhelix.module.howtomapapi.ui.home
 
 import android.content.Context
-import com.greenhelix.module.howtomapapi.R
 import android.graphics.Color
-import android.graphics.PointF
 import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ProgressBar
-import android.widget.TextView
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.greenhelix.module.howtomapapi.R
 import com.greenhelix.module.howtomapapi.databinding.FragmentNaverMapBinding
+import com.greenhelix.module.howtomapapi.databinding.WindowInfoCustomBinding
 import com.naver.maps.geometry.LatLng
-import com.naver.maps.map.*
+import com.naver.maps.map.CameraPosition
+import com.naver.maps.map.MapFragment
+import com.naver.maps.map.NaverMap
+import com.naver.maps.map.OnMapReadyCallback
 import com.naver.maps.map.overlay.InfoWindow
 import com.naver.maps.map.overlay.Marker
 import com.naver.maps.map.util.FusedLocationSource
@@ -29,7 +30,7 @@ class NaverMapFragment : Fragment(), OnMapReadyCallback{
     private lateinit var mapViewModel: MapViewModel
     private var _binding: FragmentNaverMapBinding? = null
     private val binding get() = _binding!!
-
+    private var contain : ViewGroup? = null
     private lateinit var locationSource: FusedLocationSource
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -53,7 +54,7 @@ class NaverMapFragment : Fragment(), OnMapReadyCallback{
 
         _binding = FragmentNaverMapBinding.inflate(inflater, container, false)
         val root: View = binding.root
-
+        contain = container
         locationSource = FusedLocationSource(this, 1000 )
 
         val mapFragment = childFragmentManager.findFragmentById(R.id.naver_map_view) as MapFragment?
@@ -81,56 +82,65 @@ class NaverMapFragment : Fragment(), OnMapReadyCallback{
         }
     }
 
-    private class InfoWindowAdapter(private val context: Context) : InfoWindow.ViewAdapter(){
-        private var rootView : View? = null
-        override fun getView(info: InfoWindow): View {
-            val view = rootView ?: View.inflate(context, R.layout.window_info_custom, null).also { rootView = it }
-            val progress = view.findViewById<ProgressBar>(R.id.show_data_progress)
+    private inner class InfoWindowAdapter(private val context: Context) : InfoWindow.ViewAdapter(){
+        var adapterBinding : WindowInfoCustomBinding? = null
+        private val cardBinding get() = adapterBinding
 
-            val text1 = view.findViewById<TextView>(R.id.card_tv_store_name)
-            val text2 = view.findViewById<TextView>(R.id.card_tv_store_describe)
+        override fun getView(info: InfoWindow): View {
+            adapterBinding = WindowInfoCustomBinding.inflate(LayoutInflater.from(context), contain, false)
+            val progress = cardBinding?.showDataProgress
+            val text1 = cardBinding?.cardTvStoreName
+            val text2 = cardBinding?.cardTvStoreDescribe
             val marker = info.marker
 
+            // 뷰바인딩 적용 안했을 때 뷰를 가져오는 방법
+//            val view = View.inflate(context, R.layout.window_info_custom, null)
+//            val view = rootView ?: View.inflate(context, R.layout.window_info_custom, null).also { rootView = it }
+//            val text1 = view.findViewById<TextView>(R.id.card_tv_store_name)
+//            val text2 = view.findViewById<TextView>(R.id.card_tv_store_describe)
+
             if(marker != null){
-
-                progress.progress = 40
-
-                text1.text = "김익환 카페"
-                text2.text = "여기는 가게 설명 샘플입니다. \n카페입니다. 여기는"
-
+                progress!!.progress = 40
+                text1!!.text = "김익환 카페"
+                text2!!.text = "여기는 가게 설명 샘플입니다. \n카페입니다. 여기는"
             }else{
-                progress.progress = 0
-                text1.text = "no 카페"
-                text2.text = "여기는 no sample입니다. \n where am i?"
-
+                progress!!.progress = 0
+                text1!!.text = "no 카페"
+                text2!!.text = "여기는 NoSample 입니다. \n where am i?"
             }
-            return view
+
+            return cardBinding!!.root
         }
     }
 
     override fun onMapReady(naverMap: NaverMap) {
         Log.d("ik", "mapOptions")
 
-
-
         val infoWindow = InfoWindow().apply {
-            anchor = PointF(0f, 1f)
-            offsetX = resources.getDimensionPixelSize(R.dimen.custom_info_window_offset_x)
-            offsetY = resources.getDimensionPixelSize(R.dimen.custom_info_window_offset_y)
             setOnClickListener {
                 close()
                 true
             }
         }
-        infoWindow.anchor = PointF(1f, 1f)
 
-        naverMap.locationSource = locationSource
+        naverMap.apply {
+            locationSource
+            cameraPosition = CameraPosition(NaverMap.DEFAULT_CAMERA_POSITION.target, NaverMap.DEFAULT_CAMERA_POSITION.zoom, 37.0, 45.0)
+            uiSettings.isCompassEnabled = true
+            uiSettings.isLocationButtonEnabled = true
+            setOnMapClickListener { _, latLng ->
+                infoWindow.position = latLng
+                infoWindow.close()
+            }
+        }
 
-        naverMap.cameraPosition = CameraPosition(NaverMap.DEFAULT_CAMERA_POSITION.target, NaverMap.DEFAULT_CAMERA_POSITION.zoom, 37.0, 45.0)
-
-        naverMap.uiSettings.isCompassEnabled = true
-
-        naverMap.uiSettings.isLocationButtonEnabled = true
+//        naverMap.locationSource = locationSource
+//
+//        naverMap.cameraPosition = CameraPosition(NaverMap.DEFAULT_CAMERA_POSITION.target, NaverMap.DEFAULT_CAMERA_POSITION.zoom, 37.0, 45.0)
+//
+//        naverMap.uiSettings.isCompassEnabled = true
+//
+//        naverMap.uiSettings.isLocationButtonEnabled = true
 
         Marker().apply {
             position = LatLng(37.5670135, 126.9783740)
@@ -162,22 +172,17 @@ class NaverMapFragment : Fragment(), OnMapReadyCallback{
             alpha = 0.5f
             map = naverMap
             setOnClickListener {
-                infoWindow.open(this)
-                infoWindow.adapter = InfoWindowAdapter(requireContext())
+                infoWindow!!.open(this)
+                infoWindow!!.adapter = InfoWindowAdapter(requireContext())
                 true
             }
         }
-
-        naverMap.setOnMapClickListener { _, cord ->
-            infoWindow.position = cord
-            infoWindow.close()
-        }
-
 
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+        InfoWindowAdapter(requireContext()).adapterBinding = null
     }
 }
